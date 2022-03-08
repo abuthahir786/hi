97e59c595e0cfbc4319eb411ae220ccbd1bfac87
abuthahir786 <101177285+abuthahir786@users.noreply.github.com>
2022-03-08 15:40:56 +0530

    Add files via upload

---
+++ TestAccount.java
@@ -0,0 +1,14 @@
+class TestAccount
+{
+      public static void main(String ar[])
+      {
+          Account a=new Account(100.00);
+          System.out.println("The Initilize Balance is:"+a.getBalance());
+          a.deposite(50.00);
+          a.withdraw(147.00);
+
+          
+
+       }
+}
+          
